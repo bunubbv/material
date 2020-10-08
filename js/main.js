eb0a302d514b7cf6cb0cdfb221b7b87a98771c08
RedGreen <mcredgreen@naver.com>
2020-10-08 23:50:31 +0900

    Update and rename main.js to Material.js

--- js/main.js
+++
@@ -1,7 +0,0 @@
-function init() {
-  const button = document.getElementsByTagName("button");
-  for (let i = 1; i < button.length; i++) {
-    button[i].setAttribute("class", "btn btn-white");
-  }
-}
-init();
